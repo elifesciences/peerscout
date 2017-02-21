5b5cbcb5c03b9d5908e74772ee6d039c51455de4
Daniel Ecer <de-code@users.noreply.github.com>
2017-02-21 15:10:23 +0000

    use history api

--- client/src/scenes/Main/index.js
+++ client/src/scenes/Main/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { createSelector } from 'reselect';
 import debounce from 'debounce';
+import createHashHistory from 'history/createHashHistory';
+import equals from 'deep-equal';
 
 import {
   LoadingIndicator,
@@ -16,14 +18,28 @@ const styles = {
   }
 }
 
+const parseSearch = search => {
+  const q = search[0] === '?' ? search.substring(1) : search;
+  const params = {};
+  q.split('&').forEach(s => {
+    const index = s.indexOf('=');
+    const name = index >= 0 ? s.substring(0, index) : s;
+    const value = index >= 0 ? decodeURIComponent(s.substring(index + 1)) : undefined;
+    params[name] = value;
+  });
+  return params;
+}
+
 class Main extends React.Component {
   constructor(props) {
     super(props);
+    this.history = createHashHistory({});
+    this.defaultSearchOptions = {
+      manuscriptNumber: '',
+      keywords: ''
+    };
     this.state = {
-      searchOptions: {
-        manuscriptNumber: '',
-        keywords: ''
-      },
+      searchOptions: this.defaultSearchOptions,
       reqId: 0
     };
 
@@ -40,18 +56,33 @@ class Main extends React.Component {
       }
     );
 
-    this.updateResultsDebounced = debounce(() => {
+    this.doUpdateResultsNow = () => {
+      this.actuallyLoading = true;
+      const resultsSearchOptions = this.state.searchOptions;
       this.getResults().then(resultsResponse => {
         console.log("resultsResponse:", resultsResponse);
+        this.actuallyLoading = false;
         this.setState({
           results: {
             potentialReviewers: resultsResponse['potential-reviewers'],
             matchingManuscripts: resultsResponse['matching-manuscripts']
           },
+          resultsSearchOptions,
+          shouldLoad: false,
           loading: false
         });
       });
-    }, 500);
+    };
+
+    this.updateResultsNow = () => {
+      this.setState(state => ({
+        ...state,
+        loading: true,
+        shouldLoad: true
+      }));
+    };
+
+    this.updateResultsDebounced = debounce(this.updateResultsNow, 500);
 
     this.updateResults = () => {
       this.setState({
@@ -61,8 +92,55 @@ class Main extends React.Component {
     };
   }
 
+  locationToSearchOptions(location, defaultSearchOptions) {
+    const params = parseSearch(location.search || '');
+    return {
+      ...defaultSearchOptions,
+      ...params
+    };
+  }
+
+  pushSearchOptions(searchOptions) {
+    const path = '/search?' +
+      Object.keys(searchOptions)
+      .filter(k => !!searchOptions[k])
+      .map(k => `${k}=${encodeURIComponent(searchOptions[k])}`)
+      .join('&');
+    if (path !== (this.history.location.pathname + this.history.location.search)) {
+      this.history.push(path);
+    }
+  }
+
+  setSearchOptions(searchOptions) {
+    if (!equals(this.state.searchOptions, searchOptions)) {
+      this.setState({
+        searchOptions
+      });
+      this.updateResults();
+    }
+  }
+
+  updateSearchOptionsFromLocation(location) {
+    this.setSearchOptions(this.locationToSearchOptions(
+      location, this.defaultSearchOptions
+    ));
+  }
+
   componentDidMount() {
-    this.updateResults();
+    this.updateSearchOptionsFromLocation(this.history.location);
+    this.unlisten = this.history.listen((location, action) => {
+      this.updateSearchOptionsFromLocation(location);
+    });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      (this.state.shouldLoad) &&
+      (!this.actuallyLoading)
+    ) {
+      this.pushSearchOptions(this.state.searchOptions);
+      this.doUpdateResultsNow();
+    }
   }
 
   updateOption(state) {
@@ -70,10 +148,7 @@ class Main extends React.Component {
   }
 
   onSearchOptionsChanged = searchOptions => {
-    this.setState({
-      searchOptions
-    });
-    this.updateResults();
+    this.setSearchOptions(searchOptions);
   }
 
   render() {
