7eed5b255e3fd88e8d8934b2b46c995943950fce
Daniel Ecer <de-code@users.noreply.github.com>
2017-08-30 14:56:52 +0100

    log update preprocessing exception

--- preprocessing/updateDataAndReload.py
+++ preprocessing/updateDataAndReload.py
@@ -6,25 +6,29 @@ NAME = 'updateDataAndReload'
 
 def main():
   logger = logging.getLogger(NAME)
-  scripts = [
-    'importDataToDatabase',
-    'convertEditorsCsv',
-    'importEarlyCareerResearchersCsv',
-    'enrichEarlyCareerResearchersInDatabase',
-    'generateTextTokens',
-    'generateLdaDocVec',
-    'generateDoc2Vec',
-    'reloadServer'
-  ]
-  if not import_module('downloadFiles').main():
-    logger.info("no files downloaded, skipping further processing")
-    return False
-  for script in scripts:
-    logger.info("running: %s", script)
-    pkg = import_module(script)
-    pkg.main()
-  logger.info("done")
-  return True
+  try:
+    scripts = [
+      'importDataToDatabase',
+      'convertEditorsCsv',
+      'importEarlyCareerResearchersCsv',
+      'enrichEarlyCareerResearchersInDatabase',
+      'generateTextTokens',
+      'generateLdaDocVec',
+      'generateDoc2Vec',
+      'reloadServer'
+    ]
+    if not import_module('downloadFiles').main():
+      logger.info("no files downloaded, skipping further processing")
+      return False
+    for script in scripts:
+      logger.info("running: %s", script)
+      pkg = import_module(script)
+      pkg.main()
+    logger.info("done")
+    return True
+  except Exception as e:
+    logger.exception(e)
+    raise e
 
 if __name__ == "__main__":
   from shared_proxy import configure_logging
