6f5d1c52892fcd6c05703acd06d51bef6e1d56ea
Daniel Ecer <de-code@users.noreply.github.com>
2018-02-07 17:23:43 +0000

    added basic doc2vec test

--- peerscout/docvec_model/Doc2VecTransformer.py
+++ peerscout/docvec_model/Doc2VecTransformer.py
@@ -6,10 +6,15 @@ NAME = 'Doc2VecTransformer'
 
 # Doc2Vec sklearn wrapper
 class Doc2VecTransformer(object):
-  def __init__(self, n_epochs=50, vec_size=100, window=10):
+  def __init__(
+    self, n_epochs=50, vec_size=100, window=10, min_count=5, workers=2, seed=1):
+
     self.n_epochs = n_epochs
     self.vec_size = vec_size
     self.window = window
+    self.min_count = min_count
+    self.workers = workers
+    self.seed = seed
     self.model = None
 
   def transform(self, X):
@@ -26,10 +31,14 @@ class Doc2VecTransformer(object):
     return self.fit_transform(X, y)
 
   def fit_transform(self, X, y=None):
+    logger = logging.getLogger(NAME)
+
     words_by_sentence = [
       s.split(' ')
       for s in X
     ]
+
+    logging.debug('words_by_sentence: %.100s...', words_by_sentence)
 
     sentences = [
       gensim.models.doc2vec.TaggedDocument(words=words, tags=[i])
@@ -39,17 +48,15 @@ class Doc2VecTransformer(object):
     model = gensim.models.Doc2Vec(
       size=self.vec_size,
       window=self.window,
-      min_count=5,
-      workers=2,
+      min_count=self.min_count,
+      workers=self.workers,
       alpha=0.025,
       min_alpha=0.025,
-      seed=0
+      seed=self.seed
     )
     self.model = model
     model.build_vocab(sentences)
     current_epoch = 0
-
-    logger = logging.getLogger(NAME)
 
     for _ in range(self.n_epochs):
       logger.info("epoch: %d, alpha: %f", current_epoch, model.alpha)
