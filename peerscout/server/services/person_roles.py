19ed41437c7170e4ff2f01d415bcc06103ba5e17
Daniel Ecer <de-code@users.noreply.github.com>
2018-02-19 10:06:54 +0000

    query database at runtime for person roles rather than loading all into memory

--- peerscout/server/services/person_roles.py
+++ peerscout/server/services/person_roles.py
@@ -1,6 +1,8 @@
 import logging
 from collections import Counter
 
+import sqlalchemy
+
 from peerscout.utils.collection import (
   iter_flatten,
   groupby_to_dict
@@ -11,35 +13,31 @@ from ...shared.database_schema import Person
 LOGGER = logging.getLogger(__name__)
 
 class PersonRoleService:
-  def __init__(self, role_by_person_id_map):
-    self._role_by_person_id_map = role_by_person_id_map
+  def __init__(self, db):
+    self._db = db
 
   @staticmethod
   def from_database(db):
-    return PersonRoleService.from_person_id_keyword_tuples(
-      db.session.query(
-        db.person_role.table.person_id,
-        db.person_role.table.role
-      ).join(
-        db.person.table,
-        db.person.table.status == Person.Status.ACTIVE
-      ).all(),
-    )
-
-  @staticmethod
-  def from_person_id_keyword_tuples(person_id_role_tuples):
-    return PersonRoleService(
-      role_by_person_id_map=groupby_to_dict(
-        person_id_role_tuples,
-        lambda x: x[0],
-        lambda x: x[1]
-      )
-    )
+    return PersonRoleService(db)
 
   def filter_person_ids_by_role(self, person_ids, role):
-    result = {
-      person_id for person_id in person_ids
-      if role in self._role_by_person_id_map.get(person_id, [])
-    } if role else person_ids
+    if not role:
+      return person_ids
+    db = self._db
+    raw_result = db.session.query(
+      db.person_role.table.person_id
+    ).join(
+      db.person.table,
+      sqlalchemy.and_(
+        db.person.table.person_id == db.person_role.table.person_id,
+        db.person.table.status == Person.Status.ACTIVE
+      )
+    ).filter(
+      sqlalchemy.and_(
+        db.person_role.table.person_id.in_(person_ids),
+        db.person_role.table.role == role
+      )
+    ).all()
+    result = set(r[0] for r in raw_result)
     LOGGER.debug('filtered person ids by role: %d -> %d (role=%s)', len(person_ids), len(result), role)
     return result
--- peerscout/server/services/person_roles_test.py
+++ peerscout/server/services/person_roles_test.py
@@ -7,7 +7,7 @@ from ...shared.database_schema import Person
 
 from .test_data import (
   PERSON_ID,
-  PERSON_ID1,
+  PERSON_ID1, PERSON_ID2,
   PERSON1
 )
 
@@ -26,6 +26,14 @@ def create_person_role_service(dataset):
 @pytest.mark.slow
 class TestPersonRoleService:
   class TestFilterPersonIdsByRole:
+    def test_should_not_filter_if_role_is_none(self):
+      dataset = {}
+      with create_person_role_service(dataset) as person_role_service:
+        assert (
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, None) ==
+          {PERSON_ID1}
+        )
+
     def test_should_include_person_with_specified_role(self):
       dataset = {
         'person': [PERSON1],
@@ -33,8 +41,19 @@ class TestPersonRoleService:
       }
       with create_person_role_service(dataset) as person_role_service:
         assert (
-          person_role_service.filter_person_ids_by_role([PERSON_ID1], ROLE_1) ==
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, ROLE_1) ==
           {PERSON_ID1}
+        )
+
+    def test_should_not_include_other_persons(self):
+      dataset = {
+        'person': [PERSON1],
+        'person_role': [{PERSON_ID: PERSON_ID2, 'role': ROLE_1}]
+      }
+      with create_person_role_service(dataset) as person_role_service:
+        assert (
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, ROLE_1) ==
+          set()
         )
 
     def test_should_not_include_person_with_other_role(self):
@@ -44,7 +63,7 @@ class TestPersonRoleService:
       }
       with create_person_role_service(dataset) as person_role_service:
         assert (
-          person_role_service.filter_person_ids_by_role([PERSON_ID1], ROLE_1) ==
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, ROLE_1) ==
           set()
         )
 
@@ -58,7 +77,7 @@ class TestPersonRoleService:
       }
       with create_person_role_service(dataset) as person_role_service:
         assert (
-          person_role_service.filter_person_ids_by_role([PERSON_ID1], ROLE_1) ==
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, ROLE_1) ==
           {PERSON_ID1}
         )
 
@@ -69,6 +88,6 @@ class TestPersonRoleService:
       }
       with create_person_role_service(dataset) as person_role_service:
         assert (
-          person_role_service.filter_person_ids_by_role([PERSON_ID1], ROLE_1) ==
+          person_role_service.filter_person_ids_by_role({PERSON_ID1}, ROLE_1) ==
           set()
         )
