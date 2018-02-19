f0b28ae0e8b6a764d3a9be214ce45b1232bc3a48
Daniel Ecer <de-code@users.noreply.github.com>
2018-02-19 10:08:05 +0000

    query keywords from database rather than loading all person keywords into memory

--- peerscout/server/services/person_keywords.py
+++ peerscout/server/services/person_keywords.py
@@ -1,9 +1,12 @@
 import logging
 from collections import Counter
 
+import sqlalchemy
+
 from peerscout.utils.collection import (
   iter_flatten,
-  groupby_to_dict
+  groupby_to_dict,
+  applymap_dict
 )
 
 from ...shared.database_schema import Person
@@ -19,25 +22,40 @@ LOGGER = logging.getLogger(__name__)
 def get_person_ids_of_person_keywords_scores(person_keyword_scores):
   return person_keyword_scores.keys()
 
-class PersonKeywordService(KeywordService):
-  def __init__(self, keyword_map, all_keywords):
-    super(PersonKeywordService, self).__init__(keyword_map, all_keywords, entity_name='person')
+class PersonKeywordService:
+  def __init__(self, db):
+    self._db = db
 
   @staticmethod
   def from_database(db):
-    return PersonKeywordService.from_person_id_keyword_tuples(
-      db.session.query(
-        db.person_keyword.table.person_id,
-        db.person_keyword.table.keyword
-      ).join(
-        db.person.table,
+    return PersonKeywordService(db)
+
+  def _query(self, columns):
+    db = self._db
+    return db.session.query(*columns).join(
+      db.person.table,
+      sqlalchemy.and_(
+        db.person.table.person_id == db.person_keyword.table.person_id,
         db.person.table.status == Person.Status.ACTIVE
-      ).all(),
+      )
     )
 
-  @staticmethod
-  def from_person_id_keyword_tuples(person_id_keyword_tuples):
-    return PersonKeywordService(
-      keyword_map=create_keyword_map_for_id_keyword_tuples(person_id_keyword_tuples),
-      all_keywords=set(keyword for _, keyword in person_id_keyword_tuples)
+  def get_all_keywords(self):
+    return set(
+      r[0] for r in
+      self._query([self._db.person_keyword.table.keyword]).distinct().all()
     )
+
+  def get_keyword_scores(self, keyword_list):
+    if not keyword_list:
+      return {}
+    num_keywords = len(keyword_list)
+    db = self._db
+    return dict((keyword, count / num_keywords) for keyword, count in self._query([
+      db.person_keyword.table.person_id,
+      sqlalchemy.func.count(db.person_keyword.table.person_id)
+    ]).filter(
+      sqlalchemy.func.lower(db.person_keyword.table.keyword).in_(
+        [s.lower() for s in keyword_list]
+      )
+    ).group_by(db.person_keyword.table.person_id).all())
